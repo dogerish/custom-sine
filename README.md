3bcc2286086d155882ce306cec5b3a98b343af0c
dogerish <luca.filmstrip@gmail.com>
2021-04-22 09:25:59 -0400

    Modify main.cpp to set pins/leds

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ## Notes
  you have to change the sleep thing for non-unix like operating systems (unistd and usleep)
+
+## leds branch
+ this branch modifies main.cpp to control some led's on gpio pins
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>    // cout
 #include "mathfuncs.h" // sin
 #include <unistd.h>    // usleep
-#define TERMINALWIDTH 209
+#include <wiringPi.h>  // pinMode digitalWrite
+#define NUMPINS 8
 
 // draw bar with length l
 void bar(int l, char stroke = '-', char end = '#')
@@ -10,12 +11,30 @@ void bar(int l, char stroke = '-', char end = '#')
 	std::cout << end << std::endl;
 }
 
+void display(const int pins[], int i, int value)
+{
+	digitalWrite(pins[i], value);
+	digitalWrite(pins[NUMPINS - 1 - i], value);
+}
+
 int main()
 {
-	float ANGINC = M_PI / 36;
+	wiringPiSetupGpio();
+	int pins[] = { 17, 18, 27, 22, 23, 24, 25, 5 };
+	// set all to output
+	for (int i = 0; i < NUMPINS; i++) pinMode(pins[i], OUTPUT);
+	float ANGINC = M_PI / 8;
+	int prev = -1;
 	for (float angle = 0; true; angle += ANGINC)
 	{
-		bar(round((sin(angle) + 1) * (TERMINALWIDTH - 1) / 2), ' ');
+		// turn off the prev one and turn on this one
+		int cur = round((sin(angle) + 1) * (NUMPINS - 1)/2);
+		if (cur != prev)
+		{
+			if (prev >= 0) display(pins, prev, 0);
+			display(pins, cur, 1);
+			prev = cur;
+		}
 		usleep(1e5);
 	}
 	return 0;
